62a2f70ed2b43a83ae55d4133bf8bcc04507b09d
dhruv4023 <dhruv20345@gmail.com>
2023-04-12 18:02:41 +0530

    test

--- client/src/components/Chat/ChatArea/Messages.jsx
+++ client/src/components/Chat/ChatArea/Messages.jsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import React from "react";
 export default function Messages({ msgLst }) {
-  console.log(msgLst);
+  // console.log(msgLst);
   return (
     <>
       {msgLst.map((m, i) => (
