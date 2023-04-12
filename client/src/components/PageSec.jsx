a7d57f7e1aec7442c23f0dd227367a026642f5b8
dhruv4023 <dhruv20345@gmail.com>
2023-04-12 16:40:06 +0530

    test

---
+++ client/src/components/PageSec.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FlexBetween from "./FlexBetween";
+import Home from "../Pages/Home/Home";
+
+const PageSec = () => {
+  return (
+    <FlexBetween>
+      <Home />
+    </FlexBetween>
+  );
+};
+
+export default PageSec;
