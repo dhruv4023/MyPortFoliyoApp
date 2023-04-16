a1cbb17fb7bb65eb5f9c0278176e7096b83b808d
dhruv4023 <dhruv20345@gmail.com>
2023-04-16 20:41:21 +0530

    x

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,25 @@
-import React, { useMemo } from "react";
-import { useSelector } from "react-redux";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "./theme";
-import { Navbar } from "./Pages/Navbar/Navbar";
-import ChatBtn from "./Components/Chat/ChatBtn";
-import PageSec from "./Components/PageSec";
-import { BrowserRouter as Router } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
+function App() {
   return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Navbar />
-        <PageSec />
-        <ChatBtn />
-      </ThemeProvider>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
