efa3e3bacfee31747c823d1c0d0a3c2cd1484d7e
dhruv <dhruv20345@gmail.com>
2024-03-03 18:22:46 +0530

    home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,17 +10,6 @@ export default function Home () {
   return (
     <FlexBetween width='100%' flexWrap={'wrap'}>
       <WidgetWrapper flexDirection={'column'} height={'90vh'} width={'100%'}>
-        <Box
-          sx={{
-            fontSize: '1.5rem',
-            textAlign: 'center',
-            fontWeight: 500
-          }}
-          className='text_home animate_charcter'
-        >
-          <UserImg size='400rem'/>
-          Hello My Name is Dhruv Patel.
-        </Box>
         <SocialLink />
       </WidgetWrapper>
     </FlexBetween>
